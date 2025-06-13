aee831fe0f4c70b51d48d3f66e07ee813f0d2505
Samuel Wayoro <samuelwayoro@gmail.com>
2025-06-13 17:53:46 +0200

    initialisation du projet

--- src/main/java/com/samydevup/myspringbatchproject/MyspringbatchprojectApplication.java
+++ src/main/java/com/samydevup/myspringbatchproject/MyspringbatchprojectApplication.java
@@ -1,6 +1,5 @@
 package com.samydevup.myspringbatchproject;
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
