44676aac0fb2b457aff7d1753d0890a4bbdb85ea
Samuel Wayoro <samuelwayoro@gmail.com>
2025-06-13 18:25:58 +0200

    add : new config package and @ComponentScan annotation to main project class

--- src/main/java/com/samydevup/myspringbatchproject/MyspringbatchprojectApplication.java
+++ src/main/java/com/samydevup/myspringbatchproject/MyspringbatchprojectApplication.java
@@ -3,9 +3,11 @@ package com.samydevup.myspringbatchproject;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableBatchProcessing
+@ComponentScan("com.samydevup.myspringbatchproject.config")
 public class MyspringbatchprojectApplication {
 
     public static void main(String[] args) {
