0add0c18ae175d99033e14d6d3f1175677c5f0eb
Samuel Wayoro <samuelwayoro@gmail.com>
2025-06-13 17:47:36 +0200

    first commit : creation du projet

---
+++ src/main/java/com/samydevup/myspringbatchproject/MyspringbatchprojectApplication.java
@@ -0,0 +1,14 @@
+package com.samydevup.myspringbatchproject;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspringbatchprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyspringbatchprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samydevup/myspringbatchproject/MyspringbatchprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samydevup.myspringbatchproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyspringbatchprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
